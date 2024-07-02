a2fea5e60d5093dfced9dabd640e4ecd3a0b000c
Dimitri Almon <dimitri.almon@gmail.com>
2024-07-02 16:31:16 +0200

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root "tasks#index"
 
   get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: 'new_tasks'
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: 'task'
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
